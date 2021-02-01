d727e54c6c4058e2cee9b284cf6b2bb13755a028
gg <qasemyq@yahoo.com>
2021-02-02 00:02:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app" class="container">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+      <TodoInput/>
+      <TodoList/>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+import TodoInput from '@/components/TodoInput.vue'
+import TodoList from '@/components/TodoList.vue'
+export default {
+  components: {
+    TodoInput,
+    TodoList,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-for="todo in allTodos" :key="todo.id">
+            <TodoItem :todo="todo"/>
+    </div>
+    </div>
+</template>
+
+
+<script>
+import TodoItem from '@/components/TodoItem.vue'
+import { mapGetters } from 'vuex'
+
+export default {
+    components:{
+        TodoItem
+    },
+    computed:{
+        ...mapGetters(['allTodos'])
+    }
+}
+</script>
